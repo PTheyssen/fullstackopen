437c39002803ddf453bea26799b8e1b7b91c01d7
Philipp Theyssen <p.theyssen@gmail.com>
2024-08-03 10:37:23 +0200

    Finish part 1 c

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,14 +1,48 @@
-const App = () => {
-  const friends = [
-    { name: 'Peter', age: 4},
-    { name: 'Maya', age: 10},
-  ]
+import { useState } from "react"
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+
+const Button = ({ onClick, text }) =>
+  <button onClick={onClick}>
+     {text}
+  </button>
+
+
+const App = (props) => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+  const decreaseByOne = () => {
+    console.log('decreasing,value before', counter)
+    setCounter(counter - 1)
+  }
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
   return (
-    <div>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
-    </div>
-  ) 
+    <>
+      <Display counter={counter} />
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
+    </>
+  )
 }
 
 export default App
