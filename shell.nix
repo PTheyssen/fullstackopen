46f31bd56ef266429c728be9178cefcde789b17f
Philipp Theyssen <p.theyssen@gmail.com>
2024-09-15 17:42:53 +0200

    Continue part 3

--- shell.nix
+++ shell.nix
@@ -3,6 +3,7 @@ pkgs.mkShell {
   # nativeBuildInputs is usually what you want -- tools you need to run
   nativeBuildInputs = with pkgs.buildPackages; [
     nodejs_20 # using nvm in nixos is not really recommended https://discourse.nixos.org/t/how-to-install-nvm-node-version-manager-in-nixos/20644
+    postman
   ];
 
   shellHook = ''
